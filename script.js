b47e965aa521558362f0c980106fe5c17d3a80c8
kieutrang222005-byte <kieutrang222005@gmail.com>
2026-03-01 20:37:06 +0700

    vanhy-upload-web

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+  const elements = document.querySelectorAll(".fade-in");
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+});
